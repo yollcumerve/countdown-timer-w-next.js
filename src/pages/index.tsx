8fd5a0f4d7c93bd3d68adc5ea8566197e3268562
merveyolcu <yollcumerve@gmail.com>
2023-01-31 17:50:30 +0300

    Countdown Timer with Next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,42 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import {useEffect, useState} from "react";
+import Link from "next/link"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+    const [urlTime, setUrlTime] = useState(true)
+    const [days, setDays] = useState(0)
+    const [hours,setHours] = useState(0)
+    const [minutes,setMinutes] = useState(0)
+    const [seconds,setSeconds] = useState(0)
+
+
+
+    useEffect(() => {
+        const target = new Date("01/02/2023 23:59:59")
+        const interval = setInterval(() => {
+            const now = new Date()
+            const difference = target.getTime() - now.getTime()
+
+            const days = Math.floor(difference/ (1000 * 60 * 60 * 24))
+            setDays(days)
+
+            const hours = Math.floor((difference%(1000 * 60 * 60 * 24))/(1000*60*60))
+            setHours(hours)
+
+            const minutes = Math.floor((difference%(1000*60*60))/(1000*60))
+            setMinutes(minutes)
+
+            const seconds = Math.floor((difference%(1000*60))/1000)
+            setSeconds(seconds)
+        }, 1000)
+
+        return () =>  clearInterval(interval)
+    },[])
+
   return (
     <>
       <Head>
@@ -14,110 +46,35 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+     <div className='timer-wrapper'>
+         <div className="timer-inner"></div>
+         <div className="timer-segment">
+             <span className="time">{days}</span>
+             <span className="label">Days</span>
+         </div>
+         <div className="timer-segment">
+             <span className="time">{hours}</span>
+             <span className="label">Hours</span>
+         </div>
+         <div className="timer-segment">
+             <span className="time">{minutes}</span>
+             <span className="label">Minutes</span>
+         </div>
+         <div className="timer-segment">
+             <span className="time">{seconds}</span>
+             <span className="label">Seconds</span>
+         </div>
+         { urlTime ? (
+             <div>
+                 <Link href="https://github.com/yollcumerve" legacyBehavior>
+                     <a>
+                         <button disabled={seconds>0 || seconds<0}>Go to GİTHUB</button>
+                     </a>
+                 </Link>
+             </div>
+         ): null}
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+     </div>
     </>
   )
 }
